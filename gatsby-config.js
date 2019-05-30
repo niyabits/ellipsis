f66ad767eccc4b8c96f19dfc7f5b85f7a2d3d098
Yash Gupta <yashguptaofficial@gmail.com>
2019-05-30 15:33:21 +0530

    Final UI Improvements

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ellipsis`,
+        short_name: `Ellipsis`,
         start_url: `/`,
         background_color: `#000000`,
         theme_color: `#000000`,

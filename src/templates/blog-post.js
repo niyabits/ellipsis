03d6e2baa984658f57e79a675e5e72fa76a30dcb
Yash Gupta <yashguptaofficial@gmail.com>
2019-06-02 15:59:25 +0530

    CSS Tweaks 3.0

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -10,41 +10,48 @@ export default function Template({ data, pageContext }) {
   const { next, prev } = pageContext
 
   return (
-    <Layout>
-      <SEO title={`${post.frontmatter.title}`} />
-      <div className="blog-template">
-        <br />
-        <hr />
-        <h1 className="blog-title">{post.frontmatter.title}</h1>
-        <h4 className="blog-info">
-          Posted by{" "}
-          <span className="blog-author">{post.frontmatter.author}</span> on{" "}
-          {post.frontmatter.date}
-        </h4>
-        <div
-          dangerouslySetInnerHTML={{ __html: post.html }}
-          className="blog-content"
-        />
-        <br />
-        <hr />
-        <div className="page-btns">
-          {prev && (
-            <div className="blog-post-prev-btn">
-              <Link to={prev.frontmatter.path}>
-                <button>Previous</button>
-              </Link>
-            </div>
-          )}
-          {next && (
-            <div className="blog-post-next-btn">
-              <Link to={next.frontmatter.path}>
-                <button>Next</button>
-              </Link>
-            </div>
-          )}
+    <div
+      style={{
+        background:
+          "linear-gradient(0deg, rgba(250,250,250,1) 0%, rgba(255,255,255,1) 100%)",
+      }}
+    >
+      <Layout>
+        <SEO title={`${post.frontmatter.title}`} />
+        <div className="blog-template">
+          <br />
+          <hr />
+          <h1 className="blog-title">{post.frontmatter.title}</h1>
+          <h4 className="blog-info">
+            Posted by{" "}
+            <span className="blog-author">{post.frontmatter.author}</span> on{" "}
+            {post.frontmatter.date}
+          </h4>
+          <div
+            dangerouslySetInnerHTML={{ __html: post.html }}
+            className="blog-content"
+          />
+          <br />
+          <hr />
+          <div className="page-btns">
+            {prev && (
+              <div className="blog-post-prev-btn">
+                <Link to={prev.frontmatter.path}>
+                  <button>Previous</button>
+                </Link>
+              </div>
+            )}
+            {next && (
+              <div className="blog-post-next-btn">
+                <Link to={next.frontmatter.path}>
+                  <button>Next</button>
+                </Link>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </Layout>
+      </Layout>
+    </div>
   )
 }
 
